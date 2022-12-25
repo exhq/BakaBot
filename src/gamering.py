8028ab0644245f19e3b2d36eaa7c09df31ff0243
echo <91651232+exhq@users.noreply.github.com>
2022-12-25 23:23:53 +0330

    bruh

--- src/gamering.py
+++ src/gamering.py
@@ -1,20 +1,25 @@
 import random
+
+
 def winner(player_choice, computer_choice):
     if player_choice == 'rock' and computer_choice == 'scissors':
-        return True
+        return 1
     elif player_choice == 'scissors' and computer_choice == 'paper':
-        return True
+        return 1
     elif player_choice == 'paper' and computer_choice == 'rock':
-        return True
+        return 1
+    elif player_choice == computer_choice:
+        return 2
     else:
-        return False
+        return 3
+
 
 async def rps(x, message):
     bruh = random.randint(0, 1)
     print(x)
     item = ["rock", "paper", "scissors"]
     if x not in item:
-        if bool(bruh):
+        if bruh == 1:
             await message.channel.send(f"""
 you chose {x}, i chose {item[bruh]}. i win.
 """)
@@ -23,15 +28,15 @@ you chose {x}, i chose {item[bruh]}. i win.
 you chose {x}, i chose {item[bruh]}. you win.
 """)
     else:
-        if winner(x, random.choice(item)):
+        if winner(x, random.choice(item) == 1):
             await message.channel.send(f"""
             you chose {x}, i chose {item[bruh]}. you win.
             """)
-        else:
+        elif winner(x, random.choice(item) == 3):
             await message.channel.send(f"""
             you chose {x}, i chose {item[bruh]}. i win.
             """)
-
-
-
-
+        else:
+            await message.channel.send(f"""
+            you chose {x}, i chose {item[bruh]}. lmao its a tie
+            """)
