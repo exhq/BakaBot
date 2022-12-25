1e84fc4a6ac530cb1f9aabfc223cfaba69f72b3a
echo <91651232+exhq@users.noreply.github.com>
2022-12-25 23:17:31 +0330

    literally a join

--- src/commandprovider.py
+++ src/commandprovider.py
@@ -4,4 +4,4 @@ import gamering
 async def commandprovider(x: str, message):
     x = x.split(" ")
     if x[0] == "rps":
-        await gamering.rps(x[1:], message)
+        await gamering.rps(" ".join(x[1:]), message)
