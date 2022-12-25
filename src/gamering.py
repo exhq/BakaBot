ecd1fdbd37054dfd98db9fac7c9fd9348b4f96cf
echo <91651232+exhq@users.noreply.github.com>
2022-12-26 00:07:08 +0330

    AAAAAAAAaaaaaaaaaa

--- src/gamering.py
+++ src/gamering.py
@@ -1,45 +1,21 @@
 import random
 
 
-def winner(player_choice, computer_choice):
-    if player_choice == 'rock' and computer_choice == 'scissors':
-        return True
-    elif player_choice == 'scissors' and computer_choice == 'paper':
-        return True
-    elif player_choice == 'paper' and computer_choice == 'rock':
-        return True
-    else:
-        return False
-
-
 async def rps(x, message):
-    item = ["rock", "paper", "scissors"]
-    bruh = random.randint(0, 2)
-    no = item[bruh]
-    if x == no:
-        await message.channel.send(f"""
-            you chose {x}, i chose {item[bruh]}. its a tie lmao.
-            """)
-        return
-    if x not in item:
-        if bool(bruh):
-            await message.channel.send(f"""
- you chose {x}, i chose {item[bruh]}. i win.
- """)
-            return
-        else:
-            await message.channel.send(f"""
- you chose {x}, i chose {item[bruh]}. you win.
- """)
-            return
+    options = ["rock", "paper", "scissors"]
+    computer_choice = options[random.randint(0, len(options)-1)]
+    computer_choice_index = options.index(computer_choice)
+    lmao = random.choice(["i win", "you win"])
+
+    if x == options[computer_choice_index]:
+        await message.channel.send(f"you chose {x}, i chose {options[computer_choice_index]}. its a tie lmao")
+    elif x == "rock" and options[computer_choice_index] == "scissors":
+        await message.channel.send(f"you chose {x}, i chose {options[computer_choice_index]}. you win")
+    elif x == "paper" and options[computer_choice_index] == "rock":
+        await message.channel.send(f"you chose {x}, i chose {options[computer_choice_index]}. you win")
+    elif x == "scissors" and options[computer_choice_index] == "paper":
+        await message.channel.send(f"you chose {x}, i chose {options[computer_choice_index]}. you win")
+    elif x in options:
+        await message.channel.send(f"you chose {x}, i chose {options[computer_choice_index]}. i win")
     else:
-        if winner(x, random.choice(item)):
-            await message.channel.send(f"""
-            you chose {x}, i chose {item[bruh]}. you win.
-            """)
-            return
-        else:
-            await message.channel.send(f"""
-            you chose {x}, i chose {item[bruh]}. i win.
-            """)
-            return
+        await message.channel.send(f"you chose {x}, i chose {options[computer_choice_index]}. " + lmao)
