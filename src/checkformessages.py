c152672dd0c0c3921ec08fb8f41d4e26f304e988
exhq <91651232+exhq@users.noreply.github.com>
2022-12-26 22:13:55 +0330

    quick fix

--- src/checkformessages.py
+++ src/checkformessages.py
@@ -14,11 +14,11 @@ async def checkformessages(message: Message):
         if time_passed < datetime.timedelta(seconds=300):
             return
 
-    if "touhou" in message.content:
+    if " touhou " in message.content:
         await message.reply("https://cdn.discordapp.com/attachments/918571405212270652/1056934396852191343/to_ho.mp4")
         return
 
-    if "rent" in message.content:
+    if " rent " in message.content:
         await message.reply("https://media.discordapp.net/stickers/1009434642123870298.png")
         return
 
