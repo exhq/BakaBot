7cbcb85ffe68844c675b8b1441b975d1a93e42dc
exhq <91651232+exhq@users.noreply.github.com>
2022-12-26 18:01:29 +0330

    when you rent

--- src/checkformessages.py
+++ src/checkformessages.py
@@ -16,6 +16,7 @@ async def checkformessages(message: Message):
     if "touhou" in message.content:
         await message.reply("https://cdn.discordapp.com/attachments/918571405212270652/1056934396852191343/to_ho.mp4")
         os.system(f"echo {int(time.time())} > timer")
+
     if "rent" in message.content:
         await message.reply(
         "https://media.discordapp.net/stickers/1009434642123870298.png")
